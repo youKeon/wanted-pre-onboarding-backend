318b1ee4a0f2818dedc4450104e7c3db5b00ca88
youKeon <yukeon97@gmail.com>
2023-08-06 03:00:26 +0900

    refactor : 회원 생성 유효성 검사 추가

--- src/main/java/com/yukeon/wantedpreonboardingbackend/member/domain/Member.java
+++ src/main/java/com/yukeon/wantedpreonboardingbackend/member/domain/Member.java
@@ -1,6 +1,7 @@
 package com.yukeon.wantedpreonboardingbackend.member.domain;
 
 import com.yukeon.wantedpreonboardingbackend.common.BaseEntity;
+import com.yukeon.wantedpreonboardingbackend.member.exception.InvalidMemberException;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -20,14 +21,25 @@ public class Member extends BaseEntity {
     @Column(nullable = false)
     private String password;
 
-    @Enumerated(EnumType.STRING)
-    private Role role;
-
     public Member(String email,
-                  String password,
-                  Role role) {
+                  String password) {
+
+        validateEmail(email);
+        validatePassword(password);
+
         this.email = email;
         this.password = password;
-        this.role = role;
+    }
+
+    private void validateEmail(String email) {
+        if (email.length() == 0) {
+            throw new InvalidMemberException("Email은 공백일 수 없습니다.");
+        }
+    }
+
+    private void validatePassword(String password) {
+        if (password.length() == 0) {
+            throw new InvalidMemberException("Password는 공백일 수 없습니다.");
+        }
     }
 }
