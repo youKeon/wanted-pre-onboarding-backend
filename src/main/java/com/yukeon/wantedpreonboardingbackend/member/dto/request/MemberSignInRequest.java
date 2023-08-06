afce52d2fc2a5fd1dc0f2ae97addb898f8bcd266
youKeon <yukeon97@gmail.com>
2023-08-07 03:10:13 +0900

    refactor : 로그인 유효성 검사 추가

--- src/main/java/com/yukeon/wantedpreonboardingbackend/member/dto/request/MemberSignInRequest.java
+++ src/main/java/com/yukeon/wantedpreonboardingbackend/member/dto/request/MemberSignInRequest.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @AllArgsConstructor
 @Getter
@@ -12,6 +13,7 @@ public class MemberSignInRequest {
     @Email
     @NotBlank(message = "공백일 수 없습니다.")
     private String email;
+    @Size(min = 8)
     @NotBlank(message = "공백일 수 없습니다.")
     private String password;
 }
