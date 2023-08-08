cb5b79012e6de42b7c096ffe8b96fe03503c54ab
youKeon <yukeon97@gmail.com>
2023-08-08 22:40:40 +0900

    test : Test 코드 profile 변경

--- src/test/java/com/yukeon/wantedpreonboardingbackend/WantedPreOnboardingBackendApplicationTests.java
+++ src/test/java/com/yukeon/wantedpreonboardingbackend/WantedPreOnboardingBackendApplicationTests.java
@@ -2,10 +2,9 @@ package com.yukeon.wantedpreonboardingbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
-@Profile("test")
+
 class WantedPreOnboardingBackendApplicationTests {
 
     @Test
--- src/test/java/com/yukeon/wantedpreonboardingbackend/common/annotation/ControllerTest.java
+++ src/test/java/com/yukeon/wantedpreonboardingbackend/common/annotation/ControllerTest.java
@@ -12,10 +12,8 @@ import com.yukeon.wantedpreonboardingbackend.post.domain.Post;
 import com.yukeon.wantedpreonboardingbackend.post.presentation.PostController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.web.servlet.MockMvc;
 
 
@@ -24,7 +22,6 @@ import org.springframework.test.web.servlet.MockMvc;
         MemberController.class,
         })
 @AutoConfigureRestDocs
-@Profile("test")
 public abstract class ControllerTest {
     @Autowired
     protected MockMvc mockMvc;
