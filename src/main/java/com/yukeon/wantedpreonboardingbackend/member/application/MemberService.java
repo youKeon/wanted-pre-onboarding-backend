2717ecc04d4a182b25b7553d5c6061ca046bc21f
youKeon <yukeon97@gmail.com>
2023-08-11 21:54:33 +0900

    refactor : 로그인 API 유효성 검사 추가

--- src/main/java/com/yukeon/wantedpreonboardingbackend/member/application/MemberService.java
+++ src/main/java/com/yukeon/wantedpreonboardingbackend/member/application/MemberService.java
@@ -2,6 +2,7 @@ package com.yukeon.wantedpreonboardingbackend.member.application;
 
 import com.yukeon.wantedpreonboardingbackend.auth.jwt.JwtTokenProvider;
 import com.yukeon.wantedpreonboardingbackend.member.domain.Member;
+import com.yukeon.wantedpreonboardingbackend.member.exception.NoSuchMemberException;
 import com.yukeon.wantedpreonboardingbackend.member.persistence.MemberRepository;
 import com.yukeon.wantedpreonboardingbackend.member.dto.request.MemberSignInRequest;
 import com.yukeon.wantedpreonboardingbackend.member.dto.request.MemberSignUpRequest;
@@ -26,6 +27,12 @@ public class MemberService {
     }
 
     public MemberSignInResponse signIn(MemberSignInRequest request) {
+        Member member = memberRepository.findByEmail(request.getEmail()).orElseThrow(
+                () -> new NoSuchMemberException("로그인에 실패했습니다.")
+        );
+        if (!passwordEncoder.matches(request.getPassword(), member.getPassword()))
+            throw new NoSuchMemberException("로그인에 실패했습니다.");
+
         String accessToken = jwtTokenProvider.generateToken(request.getEmail());
         return MemberSignInResponse.from(accessToken);
     }
